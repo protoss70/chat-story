6656d2b6554f5f1629504481da18861906f016e8
protoss70 <tlegendgaming@gmail.com>
2023-12-22 17:42:17 +0100

    small temporary bug fix

--- src/app/main.js
+++ src/app/main.js
@@ -3,16 +3,16 @@ import { getCookie } from "./cookie.fnc";
 import { v4 as uuidv4 } from "uuid";
 
 let deviceId = getCookie("deviceId");
+deviceId = "16qt2ccr3uk"; // Bug fix needed, doesn't work when deployed!
+
+console.log(deviceId);
 
 export default function createBot(clientCallback){
     const bot = Bot(
         `https://core.flowstorm.ai`,
         deviceId, // sender
         false, // autostart
-        {
-            ...clientCallback,
-            getUUID: () => uuidv4(),
-        },
+        {...clientCallback, getUUID: () => uuidv4(),},
         false, // called from Kotlin
         'mp3' // file type
     );
